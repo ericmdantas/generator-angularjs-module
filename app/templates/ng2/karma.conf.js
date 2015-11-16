d8b465052124c14307350acd70073c71ee1e7907
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-11-15 22:10:39 -0200

    starts to add tests for ng2

--- app/templates/ng2/karma.conf.js
+++ app/templates/ng2/karma.conf.js
@@ -1,75 +1,44 @@
-// Karma configuration
-// Generated on Wed Dec 24 2014 19:30:10 GMT-0200 (Horário brasileiro de verão)
-
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
-    // list of files / patterns to load in the browser
     files: [
-      'node_modules/angular2/bundles/angular2.js',
+      // paths loaded by Karma
+      {pattern: 'node_modules/systemjs/dist/system.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/angular2.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/testing.js', included: true, watched: true},
+      {pattern: 'karma-test-shim.js', included: true, watched: true},
 
-      'src/**/*.js',
+      // paths loaded via module imports
+      {pattern: 'src/**/*.js', included: false, watched: true},
 
-      'tests/*_test.js'
+      // paths loaded via Angular's component compiler
+      // (these paths need to be rewritten, see proxies section)
+      {pattern: 'src/**/*.html', included: false, watched: true},
+      {pattern: 'src/**/*.css', included: false, watched: true},
+
+      // paths to support debugging with source maps in dev tools
+      {pattern: 'src/**/*.ts', included: false, watched: false},
+      {pattern: 'src/**/*.js.map', included: false, watched: false},
+
+      {pattern: 'tests/**/*_test.js', included: false, watched: false}
     ],
 
-
-    // list of files to exclude
-    exclude: [],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/**/*.js': 'coverage'
+    // proxied base paths
+    proxies: {
+      // required for component assests fetched by Angular's compiler
+      "/app/": "/base/src/"
     },
 
-    coverageReporter:
-    {
-      type: 'lcov',
-      dir: 'coverage'
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-
-    // web server port
+    reporters: ['progress'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome', 'IE', 'Safari', 'Firefox', 'FirefoxNightly', 'ChromeCanary'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
-};
+    browsers: ['Chrome'],
+    singleRun: true
+  })
+}
--- app/templates/ng2/tests/_app_test.ts
+++ app/templates/ng2/tests/_app_test.ts
@@ -1,3 +1,18 @@
-describe('my awesome app', () => {
+import {
+  describe,
+  it,
+  expect,
+  inject,
+  injectAsync,
+  beforeEachProviders
+} from 'angular2/testing';
 
+describe('my awesome app', () => {
+  beforeEachProviders(() => {});
+
+  describe('creation', () => {
+    it('should be true', () => {
+      expect(true).toBe(true);
+    })
+  });
 });
